57ddee5a3e850d25801cbbfd3042821f8ca1e456
Ran Suari <ransuari@gmail.com>
2020-04-21 14:52:45 +0300

    added command argument input

--- numbers.c
+++ numbers.c
@@ -5,30 +5,63 @@
 
 #define MAX_STRING 100
 
-typedef struct { /*store the next number*/
+typedef struct { /*store the next number digits*/
     int tens;
     int ones;
-    int exit;
 }Num;
 
-char * read_next (Num *, char *); /*helper function to read the initial command*/
-void print_next (Num *);
-int main () {
-    Num next_num;
-    char inp_row[MAX_STRING];
-    char *p;
+char * read_next (Num *, char *); /*read next number, store results on struct and return string pointer */
+void print_next (Num *, FILE *); /*get the current number and print it to the output location*/
+
+
+int main (int argc, char *argv[]) {
+    
+    FILE *inp; /*input file pointer*/
+    FILE *out; /*output file pointer*/
+
+    
+    Num next_num; /*Next number struct*/
+    char inp_row[MAX_STRING]; /*array to store the input line*/
+    char *p; /*pointer to the input string array*/
     p = inp_row;
 
-    next_num.exit = -1;
-    next_num.ones = -1;
-    next_num.tens = -1;
-    next_num.tens = -1;
+        switch (argc){ /*set the input/output based on command line arguments*/
+        case 1: /*if no arguments passed, use the defult stdin/stdout */
+            inp = stdin;
+            out = stdout;
+            break;
+        case 2: /*if one argument passed, use the argument as input and defualt output*/
+            if (!(inp = fopen (argv[1],"r"))) {
+                fprintf (stderr,"\nerror reading file %s\n", argv[1]);
+                exit (0);
+                }
+            out = stdout;
+            break;
+        case 3: /*if two arguments passed, use them as input and output*/
+            if (!(inp = fopen (argv[1],"r"))) {
+                fprintf (stderr,"\nerror reading file %s\n", argv[1]);
+                exit (0);
+                }
+            if (!(out = fopen (argv[2],"w"))) {
+                fprintf (stderr,"\nerror opening file %s for writing\n", argv[2]);
+                exit (0);
+                }
+                break;
+        default: /*if more than 2 arguments passed, send error to stderr*/
+            fprintf (stderr,"\nInvalid number of parameters\n");
+            exit (0);
+        }
+    
+    // next_num.ones = -1;
+    // next_num.tens = -1;
+
     printf ("Read Number : ");
-    fgets(inp_row, MAX_STRING, stdin);
+    fgets(inp_row, MAX_STRING, inp);
+    printf ("The string is: %s", inp_row);
     while (1) {
         p = read_next(&next_num, p);
-        print_next (&next_num);
-        if (next_num.exit == 1) break;
+        print_next (&next_num, out);
+        if (!p) break;
     }
     
 }
@@ -43,7 +76,7 @@ char * read_next (Num *next, char *p){
             ;
         --p;
         if (c==EOF || c=='\n') {
-            next->exit =1; 
+            // next->exit =1; 
             return NULL;
         }
         while (isdigit(temp_str[i] = c = *p)) { /*keep reading numbers*/
@@ -55,7 +88,7 @@ char * read_next (Num *next, char *p){
         next->tens = (int) ((temp_num-next->ones)%100)/10;
 
         if (c==EOF || c=='\n') {
-            next->exit =1; 
+            // next->exit =1; 
             return NULL;
         }
         if (c==' ') return p;
@@ -67,25 +100,21 @@ char * read_next (Num *next, char *p){
 
 }
 
-void print_next (Num *next){
+void print_next (Num *next, FILE *out){
     int t = next->tens;
     int o = next->ones;
     const char ones[10][10] = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    const char tens[8][10] = {"twenty", "thirty", "fourty", "fivty", "sixty", "seventy", "eighty", "ninety"};
+    const char tens[8][10] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     const char teens[10][10] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     switch (t){
         case 0:
-            printf ("\n%s",ones[o]);
+            fprintf (out,"\n%s",ones[o]);
             break;
         case 1:
-            printf ("\n%s",teens[o]);
+            fprintf (out,"\n%s",teens[o]);
             break;
         default:
-            printf ("\n%s",tens[t-2]); 
-            if (o!=0) printf (" %s",ones[o]); 
+            fprintf (out,"\n%s",tens[t-2]); 
+            if (o!=0) fprintf (out," %s",ones[o]); 
     }
-
-
-
-
 }
