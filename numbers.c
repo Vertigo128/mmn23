13e1d7535e487063d1be43499f6c2e9ae873615c
Ran Suari <ransuari@gmail.com>
2020-04-21 15:09:49 +0300

    update remarks and ignore *.txt

--- numbers.c
+++ numbers.c
@@ -11,7 +11,7 @@ typedef struct { /*store the next number digits*/
 }Num;
 
 char * read_next (Num *, char *); /*read next number, store results on struct and return string pointer */
-void print_next (Num *, FILE *); /*get the current number and print it to the output location*/
+void print_next (Num , FILE *); /*get the current number and print it to the output location*/
 
 
 int main (int argc, char *argv[]) {
@@ -51,69 +51,73 @@ int main (int argc, char *argv[]) {
             fprintf (stderr,"\nInvalid number of parameters\n");
             exit (0);
         }
-    
-    // next_num.ones = -1;
-    // next_num.tens = -1;
-
+   
     printf ("Read Number : ");
-    fgets(inp_row, MAX_STRING, inp);
-    printf ("The string is: %s", inp_row);
+    fgets(inp_row, MAX_STRING, inp); /*use the custom inp as input location*/
+    printf ("The string is: %s", inp_row); /*print current string to screen*/
     while (1) {
-        p = read_next(&next_num, p);
-        print_next (&next_num, out);
+        p = read_next(&next_num, p); /*call function to read next line and return the next number pointer*/
+        print_next (next_num, out); /*call function to print current number as words*/
         if (!p) break;
     }
     
 }
 
+/* The function get Num struct pointer and char pointer, 
+read the next number and store the results on the struct,
+send back pointer to the next digit on the string and NULL if EOF*/
+
 char * read_next (Num *next, char *p){
-    char temp_str[MAX_STRING];
-    int temp_num;
+    char temp_str[MAX_STRING]; /*temp string to store current number*/
+    int temp_num; /*temp int to store current number as int*/
     int i=0;
-    char c;
-    if (!p) return p;
-        while ((c = *p++ == ' ' || c=='\t') )
+    int c;
+    if (!p) return p; 
+        while ((c = *p++ == ' ' || c=='\t') ) /*keep skipping next whitespaces*/
             ;
         --p;
-        if (c==EOF || c=='\n') {
-            // next->exit =1; 
+        if (c==EOF || c=='\n') { /*exit if EOF/EOL reched*/
             return NULL;
         }
-        while (isdigit(temp_str[i] = c = *p)) { /*keep reading numbers*/
+        while (isdigit(temp_str[i] = c = *p)) { /*keep reading numbers and store at temp_str*/
             i++;
             p++;
         }
-        temp_num = atoi (temp_str);
-        next->ones = (int) temp_num%10;
-        next->tens = (int) ((temp_num-next->ones)%100)/10;
+        temp_num = atoi (temp_str); /*convert the temp_str to int*/
+        next->ones = (int) temp_num%10; /*calculate and store the ones digit*/
+        next->tens = (int) ((temp_num-next->ones)%100)/10; /*calculate and store the tens digit*/
 
-        if (c==EOF || c=='\n') {
-            // next->exit =1; 
+        if (c==EOF || c=='\n') { /*exit if EOF/EOL */
             return NULL;
         }
-        if (c==' ') return p;
-        while ((c = *p++ == ' ' || c=='\t') )
-            ;
-        return p--;
+        else if (c==' ') return p;  /*return pointer for next number*/
+        else { /*send error if input is invalid*/
+            fprintf (stderr,"\nInvalid input\n");
+            exit (0);
+        }
 
 
 
 }
 
-void print_next (Num *next, FILE *out){
-    int t = next->tens;
-    int o = next->ones;
+/*The function recieve Num struct with current number data and output file pointer
+the fuction print the number as words to the output file location*/
+
+void print_next (Num next, FILE *out){
+    int t = next.tens; /*t store the tens digit*/
+    int o = next.ones; /*o store the ones digit*/
     const char ones[10][10] = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     const char tens[8][10] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     const char teens[10][10] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
     switch (t){
-        case 0:
+        case 0: /*if number is 0-9, use ones array for output*/
             fprintf (out,"\n%s",ones[o]);
             break;
-        case 1:
+        case 1: /*if number if 10-19 use teens array for output*/
             fprintf (out,"\n%s",teens[o]);
             break;
-        default:
+        default: /*on all other cases use tens and ones for output*/
             fprintf (out,"\n%s",tens[t-2]); 
             if (o!=0) fprintf (out," %s",ones[o]); 
     }
